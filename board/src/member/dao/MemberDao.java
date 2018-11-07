95164bef15bc89ca8136fc6ecfbbc7be0803c139
meme2367 <meme91322367@gmail.com>
2018-11-07 13:41:13 +0900

    board

--- board/src/member/dao/MemberDao.java
+++ board/src/member/dao/MemberDao.java
@@ -59,4 +59,5 @@ public class MemberDao {
 			pstmt.executeUpdate();
 		}
 	}
+	
 }
--- board/src/member/model/Member.java
+++ board/src/member/model/Member.java
@@ -35,4 +35,8 @@ public class Member {
 	public boolean matchPassword(String pwd) {
 		return password.equals(pwd);
 	}
+	
+	public void changePassword(String newPwd) {
+		this.password = newPwd;
+	}
 }
